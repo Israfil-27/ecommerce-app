568768f065c341422687d2adcfcbcc6ee8cc7399
Israfil666 <113192535+Israfil666@users.noreply.github.com>
2024-02-03 00:51:08 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter ,Routes ,Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './pages/home';
+import Home from './pages/Home';
 
 function App() {
   return (
